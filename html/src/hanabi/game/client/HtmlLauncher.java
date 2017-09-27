f132c8c83c59be886572b078941cc58166d648e8
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-27 19:43:06 +0700

    add project

---
+++ html/src/hanabi/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package hanabi.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import hanabi.game.GameManager;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameManager();
+        }
+}
